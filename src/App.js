d3e657930003e30e1ec6bc3ce54b66416fce09e7
dmaberlin1 <dmaberlin77@gmail.com>
2022-10-11 16:40:51 +0300

    Get Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Costs from "./components/Costs/Costs";
+import NewCost from "./components/NewCost/NewCost";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//test
+
+const App = () => {
+    const costs = [
+        {
+            date: new Date(2022, 2, 12),
+            description: 'Системный блок',
+            amount: 1100.99
+        },
+        {
+            date: new Date(2022, 11, 25),
+            description: ' AMD Ryzen 5',
+            amount: 120
+
+        },
+        {
+            date: new Date(2022, 8, 15),
+            description: 'Стул',
+            amount: 100
+
+        }
+
+
+    ]
+
+    return (
+        <div>
+            <NewCost></NewCost>
+            <Costs costs={costs}></Costs>
+        </div>
+    );
 }
 
 export default App;
