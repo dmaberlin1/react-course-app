e38b82383405de5940fe7d60a3a1f12eafe0d6e8
dmaberlin1 <dmaberlin77@gmail.com>
2022-10-12 22:29:27 +0300

    fix: INITIAL_COSTS
    Project version: 2
    
    Signed-off-by: dmaberlin1 <dmaberlin77@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import React, {useState} from "react";
 const INITIAL_COSTS = [
     {
         id: 'c1',
-        date: new Date(2022, 2, 12),
+        date: new Date(2021, 2, 12),
         description: 'Video AMD 4GB',
         amount: 1100.99
     },
     {
         id: 'c2',
-        date: new Date(2022, 11, 25),
+        date: new Date(2021, 11, 25),
         description: ' AMD Ryzen 5',
         amount: 120
 
     },
     {
         id: 'c3',
-        date: new Date(2022, 8, 15),
+        date: new Date(2021, 8, 15),
         description: 'Monitor 30"',
         amount: 100
 
